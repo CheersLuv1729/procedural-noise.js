f4e59d725987321723f52834bc93fda425c47731
CheersLuv1729 <49064449+CheersLuv1729@users.noreply.github.com>
2022-10-22 01:28:06 +0100

    export mersenne twister module

--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,4 @@
 export {Noise2d, Noise3d, Noise4d} from "./interface";
 export {Perlin3d} from "./perlin";
 export {Simplex2d, Simplex3d} from "./simplex";
+export {MT19937} from "./mersenne_twister";
